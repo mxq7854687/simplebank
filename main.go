a3e9f7165125d25c4b9821719fc7b5a726cd2064
mxq7854687 <mxq7854687@gmail.com>
2022-05-08 13:12:30 +0800

    tutorial-12

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"samplebank/api"
+	db "samplebank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:admin@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("DB Connection [ Failed ]: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
