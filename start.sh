6f8e7d0c68123a8e1df0994e1d45cb71fc7c3040
mxq7854687 <mxq7854687@gmail.com>
2022-05-16 20:10:32 +0800

    test

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ set -e
 echo "Run db migration..."
 /app/migrate --version
 cat /app/app.env
-source /app/app.env
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+# source /app/app.env
+# /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "Start the app"
 exec "$@"
